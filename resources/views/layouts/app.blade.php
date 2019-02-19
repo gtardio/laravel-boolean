17c814214a7f05566737330ccb43ad120be450de
GianlucaTardio <Gianluca.Tardio@gmail.com>
2019-02-19 19:42:11 +0100

    added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,13 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
-    
+    @include('partials._header')
+
+    @yield('content')
+
+    <footer>
+      
+    </footer>
   </body>
 
 </html>
